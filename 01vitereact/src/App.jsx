3ad69ca981a77caa80ef1dad0baeff3a74323a20
Ronit Bhattacharjee <bronit381@gmail.com>
2025-12-24 22:29:51 +0530

    done basics and created own element

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,13 @@
-import Chai from "./test1"
+import Chai from "./test1";
 function App() {
+  const username = "RBslayer";
+
   return (
     <>
-    <Chai/>
+      <Chai />
+      <h1>React with vite bundeler by {username}</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,6 +1,37 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom app!</h1>
+        </div>
+    )
+}
+
+// const ReactElement = {
+//     type:'a',
+//     props:{
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+const anotherUser = "RB"
+
+const reactElement = React.createElement(
+    'a',
+    {href:"https://google.com" , target: '_blank'},
+    'click me to visit google',
+    anotherUser
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    reactElement
 )
